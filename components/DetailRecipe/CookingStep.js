b6931b005c2796fbd2a8310a4aeed6a9bf4d258c
aminmochtarclear <aminmr.mochtar0123@gmail.com>
2020-12-12 19:10:49 +0700

    screen detail recipe

---
+++ components/DetailRecipe/CookingStep.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function CookingStep(props) {
+    return (
+        //looping disini
+        <View style={{flex:1, alignContent: 'center', justifyContent: 'center', alignItems: 'center'}}>
+            <Text>1. bagaimana</Text>
+            <Text>2. bagaimana</Text>
+        </View>
+    )
+}
